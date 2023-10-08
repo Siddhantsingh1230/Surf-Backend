44ae96b17e9ac763ea00b0b2238c40f8404f30b8
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-10-08 13:12:57 +0530

    modified orders.jsmodel

--- models/orders.js
+++ models/orders.js
@@ -1,37 +1,60 @@
 import mongoose from "mongoose";
 
-
 const ordersSchema = mongoose.SchemaTypes({
-    checkoutEmail : {
-        type : String,
-        required : true,
-    },
-    billingaddress : {
-        type : String,
-        required : true,
-    },
-    billingstate : {
-        type : String,
-        required : true,
-    },
-    billingzip : {
-        type : Number,
-        required : true,
-    },
-    userId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "users",
-        required : true
-    },
-    paymentMethod : {
-        type : String,
-        required : true,
-    },
-    cart : {
-        type : [mongoose.Schema.Types.Mixed],
-        required : true
-    }
-})
+  checkoutEmail: {
+    type: String,
+    required: true,
+  },
+  billingaddress: {
+    type: String,
+    required: true,
+  },
+  billingstate: {
+    type: String,
+    required: true,
+  },
+  billingzip: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    type: [mongoose.Schema.Types.Mixed],
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  cardholder: {
+    type: String,
+    default: "",
+  },
+  cardno: {
+    type: String,
+    default: "",
+  },
+  creditexpiry: {
+    type: String,
+    default: "",
+  },
+  cardcvv: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+});
 
-
-export const  ordersModel = mongoose.Model("orders",ordersSchema)
+export const ordersModel = mongoose.Model("orders", ordersSchema);
